5eb380d31f225e2f113c274c9430825188dcec6c
cbaroni0 <baroni128@gmail.com>
2018-05-30 22:32:40 -0700

    added commenting to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,15 @@
-/**********************************************************
- * AUTHOR 		   : Chris Baroni
- * STUDENT ID      : 266549
- * ASSIGNMENT	   : 6
- * CLASS 		   : CS1D
- * SECTION 		   : M/W 4:30p-7:50p
- * DUE DATE 	   : 3/2/2016
- **********************************************************/
+/********************************************
+* Names: Chris Baroni, Nick Bernstein, Pierce Findlay
+* CS 311 MTW 5pm-8pm
+********************************************/
 
 #include "header.h"
 
 int main()
 {
-	//OUT - outputs header
+	//outputs header
 	PrintHeader("Priority Queues", "Chris Baroni", "266549", 'a', 6);
-
+	//outputs a quick explanation of how the code works
 	cout << "/*********************************************************\n"
 		 << "* This code implements a custom Priority Queue object\n"
 		 << "* and the STL Priority Queue object in the form of a\n"
@@ -24,8 +20,8 @@ int main()
 		 << "* patient\n"
 		 << "*********************************************************/\n\n";
 
-	PriorityQueue *hospital = new PriorityQueue();
-	priority_queue<node> pq;
+	PriorityQueue *hospital = new PriorityQueue(); //priority queue using object I made
+	priority_queue<node> pq; //stl priority queue
 	int hour;	//counters used to keep track
 	int minute;	//of time
 	hour = 12;
@@ -59,16 +55,14 @@ int main()
 
 
 	/***********************************************************************
-	 * BEGINNING OF PRIORITY QUEUE TEST
-	 *
-	 * CALC & OUT - goes through entire hospital wait list and remove each
+	 * Purpose: goes through entire hospital wait list and remove each
 	 * patient after 20 minutes. also checks for emergencies
 	 ***********************************************************************/
 	cout << "*** BEGINNING PRIORITYQUEUE OBJECT TEST ***\n";
 	while(!hospital->empty())
 	{
 
-		//OUT - outputs hospital waiting list
+		//outputs hospital waiting list
 		cout << "Wait List\n";
 		cout << left << setw(16) << "Name" << "Waiting(hours)\n";
 		for(int i = hospital->size()-1; i > -1; i--)
@@ -77,9 +71,9 @@ int main()
 				 << setw(14) << hospital->at(i).priority << endl;
 		}
 
-		//CALC & OUT - for loop keeps track of the 20 minutes
-		//	that each patient experiences and checks for
-		//	emergency patients arriving
+		//for loop keeps track of the 20 minutes
+		//that each patient experiences and checks for
+		//emergency patients arriving
 		cout << "\nNow treating " << hospital->top().name << endl;
 		for(int i = 0; i < 20; i++)
 		{
@@ -94,7 +88,7 @@ int main()
 				{
 					cout << 20 - i << " ";
 
-					//CALC - increments time
+					//increments time
 					minute++;
 					if(minute == 60)
 					{
@@ -102,9 +96,9 @@ int main()
 						//if statement for hour == 12
 						(hour == 12) ? hour = 1 : hour++;
 
-					}//end IF
-				}//end FOR
-				//OUT - states when a patient finished their treatment
+					}//endIF
+				}//endFOR
+				//states when a patient finished their treatment
 				cout << endl << hospital->top().name << " treatment completed at "
 					 << hour << ":";
 				if(minute == 0)
@@ -125,7 +119,7 @@ int main()
 				{
 					cout << 20 - i << " ";
 
-					//CALC - increments time
+					//increments time
 					minute++;
 					if(minute == 60)
 					{
@@ -133,9 +127,9 @@ int main()
 						//if statement for hour == 12
 						(hour == 12) ? hour = 1 : hour++;
 
-					}//end IF
-				}//end FOR
-				//OUT - states when a patient ended their treatment
+					}//endIF
+				}//endFOR
+				//tates when a patient ended their treatment
 				cout << endl << hospital->top().name << " treatment completed at "
 							 << hour << ":";
 						if(minute == 0)
@@ -148,17 +142,17 @@ int main()
 
 			cout << 20 - i << " ";
 
-			//CALC - increments time
+			//increments time
 			minute++;
 			if(minute == 60)
 			{
 				minute = 0;
 				//if statement for hour == 12
 				(hour == 12) ? hour = 1 : hour++;
-			}//end IF
-		}// end FOR
+			}//endIF
+		}// endFOR
 
-		//OUT - states when patient finished their treatment
+		//states when patient finished their treatment
 		cout << endl << hospital->top().name << " treatment completed at "
 			 << hour << ":";
 		if(minute == 0)
@@ -166,26 +160,18 @@ int main()
 		else
 			cout << minute << "\n\n\n";
 		hospital->pop();
-	}// end WHILE
-
+	}// endWHILE
 	cout << "ALL PATIENTS TREATED\n";
 	cout << "*** ENDING PRIORITYQUEUE OBJECT TEST ***\n\n\n";
-	/***********************************************************************
-	 * END OF PRIORITY QUEUE TEST
-	 ***********************************************************************/
+	/***********************************************************************/
 
 
 	//reininitialize time
 	hour = 12;
 	minute = 0;
 	/***********************************************************************
-	 * BEGINNING OF priority_queue STL TEST
-	 *
-	 * CALC & OUT - goes through entire pq wait list and remove each
+	 * Purpose: goes through entire pq wait list and remove each
 	 * patient after 20 minutes. also checks for emergencies
-	 *
-	 *    *** Hospital wait list won't be show for this test    ***
-	 *    *** functionality not available on stl priority_queue ***
 	 ***********************************************************************/
 	cout << "*** BEGINNING priority_queue STL OBJECT TEST ***\n";
 	while(!pq.empty())
@@ -209,7 +195,7 @@ int main()
 				{
 					cout << 20 - i << " ";
 
-					//CALC - increments time
+					//increments time
 					minute++;
 					if(minute == 60)
 					{
@@ -217,9 +203,9 @@ int main()
 						//if statement for hour == 12
 						(hour == 12) ? hour = 1 : hour++;
 
-					}//end IF
-				}//end FOR
-				//OUT - states when a patient finished their treatment
+					}//endIF
+				}//endFOR
+				//states when a patient finished their treatment
 				cout << endl << pq.top().name << " treatment completed at "
 					 << hour << ":";
 				if(minute == 0)
@@ -242,7 +228,7 @@ int main()
 				{
 					cout << 20 - i << " ";
 
-					//CALC - increments time
+					//increments time
 					minute++;
 					if(minute == 60)
 					{
@@ -250,9 +236,9 @@ int main()
 						//if statement for hour == 12
 						(hour == 12) ? hour = 1 : hour++;
 
-					}//end IF
-				}//end FOR
-				//OUT - states when a patient ended their treatment
+					}//endIF
+				}//endFOR
+				//states when a patient ended their treatment
 				cout << endl << pq.top().name << " treatment completed at "
 							 << hour << ":";
 						if(minute == 0)
@@ -265,17 +251,17 @@ int main()
 
 			cout << 20 - i << " ";
 
-			//CALC - increments time
+			//increments time
 			minute++;
 			if(minute == 60)
 			{
 				minute = 0;
 				//if statement for hour == 12
 				(hour == 12) ? hour = 1 : hour++;
-			}//end IF
-		}// end FOR
+			}//endIF
+		}// endFOR
 
-		//OUT - states when patient finished their treatment
+		//states when patient finished their treatment
 		cout << endl << pq.top().name << " treatment completed at "
 			 << hour << ":";
 		if(minute == 0)
@@ -283,16 +269,19 @@ int main()
 		else
 			cout << minute << "\n\n\n";
 		pq.pop();
-	}// end WHILE
+	}// endWHILE
 
 	cout << "ALL PATIENTS TREATED\n";
 	cout << "*** ENDING priority_queue STL OBJECT TEST ***\n";
-	/***********************************************************************
-	 * END OF priority_queue STL TEST
-	 ***********************************************************************/
+	/***********************************************************************/
 	return 0;
 }
 
+/********************************************
+* initializeHospital
+* Purpose: function used to initialize the
+* stl priority queue
+********************************************/
 void initializeHospital(PriorityQueue& list)
 {
 	list.push(3, "Alice Ailment");
